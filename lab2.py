5b0874cf38e173cb2799dffb6f35749b15572583
TomasKvaraciejus <tom.kvaraciejus@gmail.com>
2024-11-22 18:59:20 +0200

    lab3.py

--- lab2.py
+++ lab2.py
@@ -6,7 +6,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-precision = 10 ** -4
+precision = 10 ** -8
 
 # funkciju apibrezimai
 
@@ -20,8 +20,7 @@ def df_dy(x, y):
     return -((x / 8) - (x ** 2 / 8) - (x * y / 4))
 
 def f_simplex(x, y):
-    # guide towards valid values
-    if x <= 0 or y <= 0:
+    if x <= 0 or y <= 0 or x >= 1 or y >= 1:
         return 1 + abs(x) + abs(y)
     return -((1 - x - y) * x * y) / 8
 
@@ -32,6 +31,7 @@ def plot(x_values, y_values):
     y_range = np.arange(-0.2, 1.4, 0.2)
     X, Y = np.meshgrid(x_range, y_range)
     Z = -f(X, Y)
+    text_offset = 0.01
 
     _, ax = plt.subplots()
     cs = ax.contour(X, Y, Z, levels=[-0.01, -0.005, -0.0025, 0, 1 / 300, 1 / 100, 1 / 50, 1 / 25, 1 / 15])
@@ -41,18 +41,19 @@ def plot(x_values, y_values):
 
     length = len(x_values)
     current = 0
-    for x, y in zip(x_values, y_values):
+    for i, (x, y) in enumerate(zip(x_values, y_values)):
         ratio = current / length
         plt.plot(x, y, marker="o", markersize=5, markeredgecolor="black", markerfacecolor=(1 - ratio, ratio, 0.5))
+        plt.text(x + text_offset, y + text_offset, i, fontsize=6, color = (1 - ratio, ratio, 0.5))
         current += 1
 
     plt.show()
 
 def run_comparisons(minimazimo_func):
     starting_points = [
-        (0.001, 0.001),
-        (0.999, 0.999),
-        (0.8, 0.8)
+        (0, 0),
+        (1, 1),
+        (0.4, 1)
         ]
 
     for x, y in starting_points:
@@ -79,21 +80,25 @@ def grad_nusileidimas(x, y):
         x_values.append(x)
         y_values.append(y)
 
+    for i,x in enumerate(x_values):
+        print(f'iteration: {i}, x: {x}')
+
     return x_values, y_values
 
 def greic_nusileidimas(x, y): 
     x_values = [x]
     y_values = [y]
+    step_sizes = []
+    internal_check_count = []
 
     cur_value = math.inf
     prev_value = math.inf
 
     while (precision < abs(prev_value - cur_value)) or (prev_value == math.inf):
-        print(prev_value - cur_value)
         grad_x = df_dx(x, y)
         grad_y = df_dy(x, y)
 
-        gamma_values = np.logspace(np.log10(0.1), np.log10(100), num=1000)
+        gamma_values = np.linspace(0, 100, num=10000)
         function_values = [f(x - gamma * grad_x, y - gamma * grad_y) for gamma in gamma_values]
         min = math.inf
         step_size = 0
@@ -102,6 +107,7 @@ def greic_nusileidimas(x, y):
                 min = function_values[i]
                 step_size = gamma_values[i]
             else:
+                internal_check_count.append(i)
                 break
 
         x -= step_size * grad_x
@@ -110,22 +116,27 @@ def greic_nusileidimas(x, y):
         x_values.append(x)
         y_values.append(y)
 
+        step_sizes.append(step_size)
+
+        print(step_size)
+
         prev_value = cur_value
         cur_value = f(x, y)
 
     return x_values, y_values
 
 def simplex(x, y):
+    alpha = 0.5
     simplex_points = [
         simplex_point(x, y),
-        simplex_point(x + 0.9659258263, y + 0.2588190451),
-        simplex_point(x + 0.2588190451, y + 0.9659258263)
+        simplex_point((x + 0.9659258263) * alpha, (y + 0.2588190451) * alpha),
+        simplex_point((x + 0.2588190451) * alpha, (y + 0.9659258263) * alpha)
     ]
 
     x_values = []
     y_values = []
 
-    for _ in range(1000):
+    for i in range(1000):
         x_values.append(simplex_points[0].x)
         y_values.append(simplex_points[0].y)
 
@@ -196,10 +207,10 @@ class lab2:
     #print("gradientinis nusileidimas:")
     #run_comparisons(grad_nusileidimas)
 
-    #print("greiciausias nusileidimas:")
-    #run_comparisons(greic_nusileidimas)
+    print("greiciausias nusileidimas:")
+    run_comparisons(greic_nusileidimas)
 
-    print("simplex:")
-    run_comparisons(simplex)
+    #print("simplex:")
+    #run_comparisons(simplex)
 
 
---
+++ lab3.py
@@ -0,0 +1,104 @@
+# Optimizacijos Metodai Lab2
+# Tomas Kvaraciejus, 2110588
+
+import math
+import numpy as np
+import numdifftools as nd
+
+class points:
+    x: float
+    y: float
+    z: float
+
+    def __init__(self, _x, _y, _z):
+        self.x = _x
+        self.y = _y
+        self.z = _z
+
+    def deduct(self, x,y,z):
+        self.x -= x
+        self.y -= y
+        self.z -= z
+
+precision = 10 ** -4
+def unwrap_to_points(xyz):
+    return points(xyz[0], xyz[1], xyz[2])
+def func(p: points):
+    return -(p.x * p.y * p.z)
+def func_from_arr(xyz):
+    return func(unwrap_to_points(xyz))
+def h_func(xyz):
+    p = unwrap_to_points(xyz)
+    return 1 - (2 * p.x * p.y) - (2 * p.x * p.z) - (2 * p.y * p.z)
+
+starting_points = [
+    points(0.0, 0.0, 0.0),
+    points(1.0, 1.0, 1.0),
+    points(0.5, 0.8, 0.8)
+    ]
+
+bandomos_baudos = [
+    (0.5, 1.2),
+    (0.75, 1.5),
+    (1.0, 2.0)
+]
+
+def greic_nusileidimas(curr_func, p: points):
+    f_old = math.inf
+    f_new = curr_func([p.x, p.y, p.z])
+
+    while precision < abs(f_old - f_new):
+        grad_x, grad_y, grad_z = nd.Gradient(curr_func)([p.x, p.y, p.z])
+        if abs(grad_x) <= 10 ** -6:
+            grad_x -= 10 ** -6
+        if abs(grad_y) <= 10 ** -6:
+            grad_y -= 10 ** -6
+        if abs(grad_z) <= 10 ** -6:
+            grad_z -= 10 ** -6
+
+        gamma_values = np.linspace(0, 100, num=10000)
+        step_size = 0
+
+        f_min = math.inf
+        f_value = math.inf
+        for gamma in gamma_values:
+            f_old = f_value
+            f_value = curr_func([p.x - gamma * grad_x, p.y - gamma * grad_y, p.z - gamma * grad_z])
+            if f_value < f_min:
+                f_min = f_value
+                step_size = gamma
+            if f_old < f_value:
+                break
+
+        p.deduct(step_size * grad_x, step_size * grad_y, step_size * grad_z)
+        f_old = f_new
+        f_new = curr_func([p.x, p.y, p.z])
+
+    return p
+
+def geriausia_bauda(p, bauda, bauda_mult):
+    f_old = math.inf
+    f_new = func(p)
+    i = 0
+
+    while precision < abs(f_old - f_new):
+        curr_func = lambda xyz: (func_from_arr(xyz) + bauda *
+                                 ((min(0, xyz[0]) ** 2 + min(0, xyz[1]) ** 2 + min(0, xyz[2]) ** 2) + h_func(xyz) ** 2))
+        p = greic_nusileidimas(curr_func, p)
+        bauda *= bauda_mult
+        f_old = f_new
+        f_new = curr_func([p.x, p.y, p.z])
+        i += 1
+    
+    return p, i
+
+class lab3:
+    for p in starting_points:
+        print("pradiniai taskai: [{:1}, {:1}, {:1}]\n".format(p.x, p.y, p.z))
+        for bauda, baudos_daugiklis in bandomos_baudos:
+            print("pradine bauda: {:1}".format(bauda), ", baudos daugiklis: {:1}".format(baudos_daugiklis))
+            p_copy = points(p.x, p.y, p.z)
+            (taskai, iteracijos) = geriausia_bauda(p_copy, bauda, baudos_daugiklis)
+            print("gauti taskai: [{:.12f}, {:.12f}, {:.12f}], gautas f-jos rezultatas: {:.12f}, iteraciju kiekis: {}\n".format(taskai.x, taskai.y, taskai.z, func(taskai), iteracijos))
+            
+        print("--------------------\n")
